670b6bad2b85e8a90e7a76f9fb0c401966d0c182
nikhil080405-star <nikhil080405@gmail.com>
2026-04-01 23:27:07 +0530

    clean project

--- ai_response.py
+++ ai_response.py
@@ -1,40 +1,28 @@
-<<<<<<< HEAD
-from groq import Groq
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.getenv("GROQ_API_KEY")
-client = Groq(api_key=api_key)
-
-def get_ai_response(prompt):
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a voice assistant. Answer in 1 or 2 short sentences only. Be clear and direct."
-            },
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=60   # 🔥 LIMIT LENGTH
-    )
-=======
 from groq import Groq
 
-client = Groq(api_key="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")
+# Load env
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=api_key)
+
 
 def get_ai_response(prompt):
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a voice assistant. Answer in 1 or 2 short sentences only. Be clear and direct."
-            },
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=60   # 🔥 LIMIT LENGTH
-    )
->>>>>>> 3fd4ff1b9efcd09783e97e4d36e9d336665ac9fe
-    return response.choices[0].message.content
+    try:
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",  # latest working model
+            messages=[
+                {"role": "system", "content": "You are a voice assistant. Answer briefly."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=60
+        )
+
+        return response.choices[0].message.content.strip()
+
+    except Exception as e:
+        print("Error:", e)
+        return "Sorry, I'm having trouble right now."
--- test_voice.py
+++ test_voice.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 from text_to_speech import speak
 
-=======
-from text_to_speech import speak
 
->>>>>>> 3fd4ff1b9efcd09783e97e4d36e9d336665ac9fe
 speak("Hello Nikhil, your assistant is working", "en")
